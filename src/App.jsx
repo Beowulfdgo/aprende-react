bebf4593ef8222d6def67359b7ae9e367b12b132
antonio <antonioflores30@hotmail.com>
2025-03-03 13:57:32 -0600

    estilos en app y en index

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+function App() {
+  return (
+   <article>
+    <img src="https://unavatar.io/x/kikobeats" alt="el avatar de kikobeats" />
+    <div>
+      <strong>Kiko Beats</strong>
+      <span>@kikobeats</span>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>
+    </div>
+
+    <aside>
+      <button>Seguir</button>
+    </aside>
+  </article>
+);
+}
+
+export default App;
