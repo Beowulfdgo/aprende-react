1f5dfd07db9909043f20c384068ed922c032246c
antonio <antonioflores30@hotmail.com>
2025-03-07 12:54:34 -0600

    props

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,13 @@
+import { Fragment } from 'react';
 import './App.css';
+import { TwitterFollowCard } from './TwitterFollowCard';
 
 function App() {
   return (
-   <article className='tw-followCard'>
-    <header className='tw-followCard-header'>
-    <img 
-    className='tw-followCard-avatar'
-    alt="el avatar de kikobeats" 
-    src="https://unavatar.io/x/kikobeats" />
-    <div className='tw-followCard-info'>
-      <strong>Kiko Beats</strong>
-      <span className='tw-followCard-infoUserName'>@kikobeats        
-      </span>
-       </div>
-    </header>
-    <aside>
-      <button className='tw-followCard-button'>Seguir</button>
-    </aside>
-  </article>
-);
+
+      <TwitterFollowCard userName="kikobeats" name="Kikobeats" />
+
+  );
 }
 
 export default App;
