d30a1f966d598d91676418c6e16497373d32219c
antonio <antonioflores30@hotmail.com>
2025-03-07 12:20:20 -0600

    estilos tipo twitter

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
+import './App.css';
+
 function App() {
   return (
-   <article>
-    <img src="https://unavatar.io/x/kikobeats" alt="el avatar de kikobeats" />
-    <div>
+   <article className='tw-followCard'>
+    <header className='tw-followCard-header'>
+    <img 
+    className='tw-followCard-avatar'
+    alt="el avatar de kikobeats" 
+    src="https://unavatar.io/x/kikobeats" />
+    <div className='tw-followCard-info'>
       <strong>Kiko Beats</strong>
-      <span>@kikobeats</span>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>
-    </div>
-
+      <span className='tw-followCard-infoUserName'>@kikobeats        
+      </span>
+       </div>
+    </header>
     <aside>
-      <button>Seguir</button>
+      <button className='tw-followCard-button'>Seguir</button>
     </aside>
   </article>
 );
