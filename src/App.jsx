e47cbf141dfbb308ebe63fe0db727746f7ece24d
antonio <antonioflores30@hotmail.com>
2025-03-10 12:44:03 -0600

    objects props

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { Fragment } from 'react';
 import './App.css';
 import { TwitterFollowCard } from './TwitterFollowCard';
 
+const kikobeats={isFollowing:true ,userName:"kikobeats"}
+const  midudev={ isFollowing:false,  userName:"midudev" }
 function App() {
   return (
     <section className='App'>
-      <TwitterFollowCard  isFollowing={true} userName="kikobeats" > 
+      <TwitterFollowCard {...kikobeats}  > 
        <h1>kiko Beats</h1> </TwitterFollowCard>  
-      <TwitterFollowCard   isFollowing={false}  userName="midudev" >
+      <TwitterFollowCard {...midudev}  >
        <h1>Miguel D esta es una prueba</h1>  </TwitterFollowCard> 
     </section>
   );
